46e6cbbc8ed2b529f19caa50f6148488f7137c60
DJLee68 <ted4562000@naver.com>
2020-10-27 17:36:12 +0900

    subset construct finish

--- finite_automata.py
+++ finite_automata.py
@@ -24,16 +24,36 @@ class FA(): #{
     #}
 
     def check_input(self, input): #{
-        pass
+        state = self.start_state[0]
+        for i in input: #{
+            if i not in self.input_symbol:#{
+                print('Rejected1!')
+                return
+            #}
+            for func in self.transition_function: #{
+                if func[0] == state and func[1] == i: #{
+                    state = func[2]
+                    break
+                #}
+            #}
+        #}
+        if state in self.final_states: #{
+            print('Accepted!')
+            return
+        #}
+        else: #{
+            print('Rejected2!')
+            return
+        #}
     #}
 
-    def get_closure(self, inp): #{
-        temp = [inp]
+    def get_closure(self, state): #{
+        temp = [state]
         # search every transition function
         for func in self.transition_function: #{
-            if inp == func[0] and 'ep' == func[1]: #{
+            if state == func[0] and 'ep' == func[1]: #{
                 for output in func[2:]: #{
-                    temp.append(self.get_closure(output))
+                    temp.extend(self.get_closure(output))
                 #}
             #}
         #}
@@ -50,15 +70,19 @@ class FA(): #{
         marked.append(list(self.get_closure(self.start_state[0])))
 
         while(unmarked): #{
+            # check unmarked
             for u in unmarked: #{
                 # check each input symbol
-                for i in self.get_closure(self.input_symbol): #{
-                    # check each transition function
+                for i in self.input_symbol: #{
                     tmp = set()
+                    # check each transition function
                     for func in self.transition_function: #{
                         # if they are in transition function
-                        if func[0] in u and i == func[1]: #{
-                            tmp.update(func[2:])
+                        if func[0] in u and i == func[1] and i != 'ep': #{
+                            # put each output's closure
+                            for output in func[2:]: #{
+                                tmp.update(tuple(self.get_closure(output)))
+                            #}
                         #}
                     #}
                     if len(tmp) != 0: #{
@@ -91,74 +115,11 @@ class ENFA(FA): #{
 class NFA(FA): #{
     def __init__(self): #{
         super().__init__()
-    #
-
-    # NFA to DFA
-    def subset_construct(self): #{
-        unmarked = []
-        marked = []
-        tmp_transition = []
-        final = []
-        unmarked.append(self.start_state[0])
-        marked.append(list(self.start_state[0]))
-        while(unmarked): #{
-            for u in unmarked: #{
-                # check each input symbol
-                for i in self.input_symbol: #{
-                    # check each transition function
-                    tmp = set()
-                    for func in self.transition_function: #{
-                        # if they are in transition function
-                        if func[0] in u and i == func[1]: #{
-                            tmp.update(func[2:])
-                        #}
-                    #}
-                    if len(tmp) != 0: #{
-                        if list(tmp) not in marked: unmarked.append(list(tmp))
-                        for final_state in self.final_states: #{
-                            if final_state in list(tmp): #{
-                                if list(tmp) not in final: final.append(list(tmp))       
-                            #}
-                        #}
-                        tmp_transition.append([u, i, list(tmp)])
-                        if list(tmp) not in marked: marked.append(list(tmp))
-                    #}
-                #}
-                unmarked.remove(u)
-            #}
-        #}
-
-        self.final_states = final
-        self.transition_function = tmp_transition
-        self.states = marked
     #}
 #}
 
 class DFA(FA): #{
     def __init__(self): #{
         super().__init__()
-    #}
-    def check_input(self, input): #{
-        state = self.start_state[0]
-        for i in input: #{
-            if i not in self.input_symbol:#{
-                print('Rejected1!')
-                return
-            #}
-            for func in self.transition_function: #{
-                if func[0] == state and func[1] == i: #{
-                    state = func[2]
-                    break
-                #}
-            #}
-        #}
-        if state in self.final_states: #{
-            print('Accepted!')
-            return
-        #}
-        else: #{
-            print('Rejected2!')
-            return
-        #}
     #}
 #}
